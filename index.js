049d45fa6c64717ca743ed705530ec219c0ebba0
ArielFeingold <feingold.ariel@gmail.com>
2018-06-12 11:36:17 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,25 @@
+function produceDrivingRange(blockRange){
+  return function(start, finish){
+    let distance = parseInt(finish) - parseInt(start)
+    if (distance > blockRange){
+      return `${Math.abs(distance - blockRange)} blocks out of range`
+    } else return `within range by ${Math.abs(distance - blockRange)}`
+  }
+}
+
+function produceTipCalculator(tip){
+  return function(amount){
+    return amount * tip
+  };
+};
+
+function createDriver(){
+  let DriverId = 0
+
+  return class {
+    constructor(name){
+      this.name = name
+      this.id = ++DriverId;
+    };
+  };
+};
